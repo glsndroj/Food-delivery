d02af295582c40474253fcc38439f7e785e07e0e
glsndroj <galsandooorj@gmail.com>
2025-10-29 12:55:38 +0800

    frontend Setup

---
+++ frontend/src/_components/Navbar.tsx
@@ -0,0 +1,12 @@
+
+
+export const Navbar = () => {
+    return(
+        <div className="header bg-[#18181B] w-full h-[172px]">
+            <div className="headerContainer">
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
