dc398cc5990d3bcee58be490e740fce22a9cf548
glsndroj <galsandooorj@gmail.com>
2025-10-29 12:55:38 +0800

    frontend Setup

---
+++ frontend/src/app/DishesCategory/page.tsx
@@ -0,0 +1,11 @@
+import { SideMenu } from "./_components/Sidemenu";
+
+
+
+export default function DishesHome () {
+    return (
+        <div className=" w-full h-screen bg-[#F4F4F5]">
+            <SideMenu/>
+        </div>
+    )
+}
