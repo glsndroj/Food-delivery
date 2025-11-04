37697506760edb7666cfad548deda8941575992e
glsndroj <galsandooorj@gmail.com>
2025-11-04 15:41:09 +0800

    login and signup ui finish, linked

---
+++ frontend/src/app/Login/page.tsx
@@ -0,0 +1,11 @@
+import { LoginUser } from "./_features/Login";
+
+
+
+export default function Home () {
+    return (
+        <div className="flex">
+            <LoginUser/>
+        </div>
+    )
+}
---
+++ frontend/src/app/signUp/page.tsx
@@ -0,0 +1,10 @@
+import { CreateUser } from "./_features/CreateUser";
+
+
+export default function Home () {
+    return (
+        <div className="flex">
+            <CreateUser/>
+        </div>
+    )
+}
