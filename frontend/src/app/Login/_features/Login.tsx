1b0d93355d703d41e096c738ae8d906bd51204c3
glsndroj <galsandooorj@gmail.com>
2025-11-04 16:04:31 +0800

    aa

--- frontend/src/app/Login/_features/Login.tsx
+++ frontend/src/app/Login/_features/Login.tsx
@@ -5,11 +5,9 @@ export function LoginUser () {
     return (
         <div className="flex gap-24 w-full h-screen items-center justify-center">
             <div className="w-[416px] h-72 flex flex-col gap-6 ">
-            
             <svg className=" shrink-0 border rounded-md border-gray-200 w-9 h-9 py-2 px-2 " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="none">
              <path d="M10 12L6 8L10 4" stroke="#18181B" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
-           
             <div>
                 <h2 className="text-2xl font-semibold">Log in</h2>
                 <p className="text-gray-400 font-normal">Sign up to explore your favorite dishes.</p>
@@ -18,7 +16,6 @@ export function LoginUser () {
             <input className="border border-gray-300 rounded-md py-1" type="text" placeholder="Enter your email address" />
             <input className="border border-gray-300 rounded-md py-1" type="password" placeholder="Enter your password" />
             </div>
-           
             <button className="bg-gray-300 text-[#FAFAFA] py-2 rounded-md">Let's Go</button>
             <div className="flex gap-3 justify-center">
                 <p className="text-[#71717A]">Don't have account?</p>
@@ -29,10 +26,5 @@ export function LoginUser () {
 
          <img className="w-7xl h-[1004px] rounded-md" src="/fooddelivery.jpg" alt="fooddelivery" />
         </div>
-          
-
-          
-        
-       
     )
 }
