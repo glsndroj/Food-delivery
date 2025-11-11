7e2780f56882d464fe3dc66b27a00a332246a2d4
glsndroj <galsandooorj@gmail.com>
2025-11-11 15:26:03 +0800

    deletion

--- frontend/src/app/signUp/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CreateUser } from "./_features/CreateUser";
-
-export default function Home () {
-    return (
-        <div className="flex">
-            <CreateUser/>
-        </div>
-    )
-}
